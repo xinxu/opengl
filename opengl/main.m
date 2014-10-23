354b2b1a628484303a3b44d7e95d133532922eb1
Xin Xu <xinxu0x@gmail.com>
2014-10-23 21:31:12 +0800

    Initial Commit

---
+++ opengl/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  opengl
+//
+//  Created by Xin Xu on 2014/10/23.
+//  Copyright (c) 2014年 Xin Xu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
